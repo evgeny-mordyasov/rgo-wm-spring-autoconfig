f1d461c571d6555a2f31a343f66b3b40419c9395
evgeny-mordyasov <michisig11i7@gmail.com>
2024-07-22 22:43:58 +0700

    add web module

---
+++ rgo-wm-spring-autoconfig-web/src/main/java/rgo/wm/spring/autoconfig/web/WebConfiguration.java
@@ -0,0 +1,10 @@
+package rgo.wm.spring.autoconfig.web;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Import;
+import rgo.wm.spring.web.ExceptionResponseAdvice;
+
+@AutoConfiguration
+@Import(ExceptionResponseAdvice.class)
+public class WebConfiguration {
+}
